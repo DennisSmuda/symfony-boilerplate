09f30db77fd1837c54691746dd193e76e164ff82
Dennis Smuda <dennissmuda@msn.com>
2016-05-05 10:53:50 +0200

    generalize - added initial assets to project

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new PhotoSiteBundle\PhotoSiteBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new SiteBundle\SiteBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
